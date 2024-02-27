7d7f7bbb70c7b9f27e8ae529db685c467e6c9192
thlee5842@gmail.com <thlee5842@gmail.com>
2024-02-27 17:58:27 +0900

    커넥션스트링 설정

---
+++ Damoyeo.Common/CookieSerializer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Web;
+
+namespace Damoyeo.Common
+{
+    public class CookieSerializer
+    {
+        // 객체를 쿠키에 직렬화하여 저장
+        /*
+        public void SerializeToCookie(object obj, string key)
+        {
+            HttpCookie encodedCookie = new HttpCookie(key);
+            MemoryStream memoryStream = new MemoryStream();
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            binaryFormatter.Serialize(memoryStream, obj);
+            byte[] byteArr = memoryStream.ToArray();
+            encodedCookie.Value = Convert.ToBase64String(byteArr);
+            HttpContext.Current.Response.Cookies.Add(encodedCookie);
+        }
+        */
+        // 쿠키에서 객체를 역직렬화하여 반환
+
+        /*
+        public object DeserializeFromCookie(string key)
+        {
+            HttpCookie encodedCookie = HttpContext.Current.Request.Cookies[key];
+            MemoryStream memoryStream = new MemoryStream(Convert.FromBase64String(encodedCookie.Value));
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            return binaryFormatter.Deserialize(memoryStream);
+        }
+        */
+    }
+}
--- Damoyeo.Web/Controllers/HomeController.cs
+++ Damoyeo.Web/Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Web;
 using System.Web.Mvc;
 
@@ -23,7 +25,15 @@ namespace Damoyeo.Web.Controllers
         {
             var dd = _logger.Test("dd");
 
+            /*
 
+            CookieSerializer cookieSerializer = new CookieSerializer();
+            User user = new User { Email = "user@example.com", Nickname = "example" };
+            // User 객체를 쿠키에 직렬화하여 저장
+            cookieSerializer.SerializeToCookie(user, "UserCookie");
+            // 쿠키에서 User 객체를 역직렬화하여 반환
+            User deserializedUser = (User)cookieSerializer.DeserializeFromCookie("UserCookie");
+            */
             /*
             using (var conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DamoyeoConnectionString"].ConnectionString)) 
             {
@@ -55,4 +65,43 @@ NAME= '이태환'
             return View();
         }
     }
+
+    public class User
+    {
+
+        public string id { get; set; }
+
+        public string email { get; set; }
+
+    }
+
+
+    public class CookieSerializer
+    {
+        // 객체를 쿠키에 직렬화하여 저장
+        public void SerializeToCookie(object obj, string key)
+        {
+            HttpCookie encodedCookie = new HttpCookie(key);
+            MemoryStream memoryStream = new MemoryStream();
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            binaryFormatter.Serialize(memoryStream, obj);
+            byte[] byteArr = memoryStream.ToArray();
+            encodedCookie.Value = Convert.ToBase64String(byteArr);
+            HttpContext.Current.Response.Cookies.Add(encodedCookie);
+        }
+
+        // 쿠키에서 객체를 역직렬화하여 반환
+        public object DeserializeFromCookie(string key)
+        {
+            HttpCookie encodedCookie = HttpContext.Current.Request.Cookies[key];
+            MemoryStream memoryStream = new MemoryStream(Convert.FromBase64String(encodedCookie.Value));
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+            return binaryFormatter.Deserialize(memoryStream);
+        }
+    }
+
+
+
 }

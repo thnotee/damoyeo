dee9eabcbbe32dbfd32ff60b4e4d5fde8c71758a
thlee5842@gmail.com <thlee5842@gmail.com>
2024-03-12 17:59:43 +0900

    모임

--- Damoyeo.Web/Content/js/common.js
+++ Damoyeo.Web/Content/js/common.js
@@ -25,22 +25,20 @@ function alertFocus(text, obj) {
     });
 }
 
-
-
 //하트 클릭시 버튼 색 변함
 $(document).ready(function () {
-  $(".heart_click").click(function () {
-    $(this)
-      .find("img")
-      .attr("src", function (_, attr) {
-        return attr === "images/heart.svg"
-          ? "images/heart_on.svg"
-          : "images/heart.svg";
-      });
-  });
+    $(".heart_click").click(function () {
+        $(this)
+            .find("img")
+            .attr("src", function (_, attr) {
+                return attr === "images/heart.svg"
+                    ? "images/heart_on.svg"
+                    : "images/heart.svg";
+            });
+    });
 
-  $(".top_fixed").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 1000);
-    return false;
-  });
+    $(".top_fixed").click(function () {
+        $("html, body").animate({ scrollTop: 0 }, 1000);
+        return false;
+    });
 });
---
+++ Damoyeo.Web/Controllers/MeetupController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Damoyeo.Web.Controllers
+{
+    public class MeetupController : Controller
+    {
+        // GET: Meetup
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Write()
+        {
+            return View();
+        }
+    }
+}
